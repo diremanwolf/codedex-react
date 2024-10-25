2f431acb421df8f6e44b5022f587bd9b23f3ba66
Dante Samael Fernández Lago <dantesamaelfernandezlago@gmail.com>
2024-10-25 17:17:43 -0500

    SS: edit color-picker's files.

--- color-picker/src/App.jsx
+++ color-picker/src/App.jsx
@@ -1,33 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [ selColor, setSelColor ] = useState({ hex: null, name: null });
+  const [ focColor, setFocColor ] = useState(null);
+
+  const colors = []
+
+  function handleButtonClick() {
+    for(let i = 0; i < 3; i++) {
+      let randomColor = Math.floor(Math.random()*16777215).toString(16);
+      
+      colors.push("#" + randomColor);
+    }
+  }
+
+  function handleClick(color) {
+    console.log(color);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>Color Picker</h1>
+
+      <button onClick={handleClick}>Pick 3 Random Colors</button>
+
+      <div className="color-picker-list">
+        { colors.map(function(color, index) {
+          return (
+            <div
+              key={index}
+              className="color-picker-list-item"
+              style={{backgroundColor: `${color.hex}`}}
+              onClick={() => handleClick(color)}
+            >
+              
+            </div>
+          );
+        }) }
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
